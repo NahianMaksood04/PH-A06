98034221b8f66a571ee68aba45d48d192203a83a
NahianMaksood04 <nahianmaksood04@gmail.com>
2025-09-09 14:36:23 +0600

    html complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hi")
+const categoryName = () => {
+	fetch("https://openapi.programming-hero.com/api/categories")
+		.then((res) => res.json())
+		.then((data) => {
+			const list = document.getElementById("category-container");
+			list.innerHTML = "";
+
+			data.categories.forEach((category) => {
+				const li = document.createElement("li");
+				li.textContent = category.category_name;
+				list.appendChild(li);
+			});
+		});
+};
+
+categoryName();
